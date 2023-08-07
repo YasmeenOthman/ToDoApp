910a03c0f371e933439c27816dfbfe06799d81de
Yasmeen Othman <yasmeen.othman20@gmail.com>
2023-08-07 14:10:19 +0300

    handle the logout and conditional rendering

--- client/src/components/NavBar/Nav.js
+++ client/src/components/NavBar/Nav.js
@@ -1,23 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
+  const navigate = useNavigate("");
+  const token = localStorage.getItem("token");
+
+  function handleLogOut() {
+    if (token) {
+      localStorage.removeItem("token");
+      navigate("/");
+    } else {
+      return;
+    }
+  }
+
   return (
     <div className="navbar">
       <Link to="/">
         <h1>Plan Your Day...</h1>
       </Link>
       <div>
-        <Link to="/Task">
-          <button className="btn">Your Tasks</button>
-        </Link>
-        <Link to="/login">
-          <button className="btn">Login</button>
-        </Link>
-        <Link to="/signup">
-          <button className="btn">SignUp</button>
-        </Link>
+        {token ? (
+          <div>
+            <Link to="/Task">
+              <button className="btn">Your Tasks</button>
+            </Link>
+            <Link to="/">
+              <button onClick={handleLogOut} className="btn">
+                LogOut
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/login">
+              <button className="btn">Login</button>
+            </Link>
+            <Link to="/signup">
+              <button className="btn">SignUp</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
